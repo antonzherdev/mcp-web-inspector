58521d50914e38d0702c9c96cac9375303c36bba
Anton Zherdev <anton.zherdev@icloud.com>
2025-10-26 18:06:19 +1300

    Fix tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,12 +7,15 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/index.ts', // exclude index.ts
+    '!src/tools/browser/ancestorInspection.ts', // exclude - uses page.evaluate
     '!src/tools/browser/elementVisibility.ts', // exclude - uses page.evaluate
     '!src/tools/browser/computedStyles.ts', // exclude - uses page.evaluate
     '!src/tools/browser/findByText.ts', // exclude - uses page.evaluate
     '!src/tools/browser/elementExists.ts', // exclude - uses page.evaluate
     '!src/tools/browser/getTestIds.ts', // exclude - uses page.evaluate
+    '!src/tools/browser/inspectDom.ts', // exclude - uses page.evaluate
     '!src/tools/browser/measureElement.ts', // exclude - uses page.evaluate
+    '!src/tools/browser/visiblePage.ts', // exclude - uses page.evaluate
   ],
   testMatch: [
     '<rootDir>/src/__tests__/**/*.test.ts'
